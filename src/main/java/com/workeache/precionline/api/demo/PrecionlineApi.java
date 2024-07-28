e5ac173cd91dc53e00c6f07bccace957f5ff2235
agutierrez <agutierrez@dguadalajara.es>
2024-07-28 19:42:07 +0200

    Pruebas para despliegue en Railway

--- src/main/java/com/workeache/precionline/api/demo/PrecionlineApi.java
+++ src/main/java/com/workeache/precionline/api/demo/PrecionlineApi.java
@@ -2,10 +2,12 @@ package com.workeache.precionline.api.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class PrecionlineApi {
 
 	@Bean
