b1c80896c2d872af2ad02f58b1be2f224ed88936
agutierrez <andres6700@gmail.com>
2024-08-06 13:58:37 +0200

    Pruebas Batch

--- src/main/java/com/workeache/precionline/api/demo/services/ApiReeServiceImpl.java
+++ src/main/java/com/workeache/precionline/api/demo/services/ApiReeServiceImpl.java
@@ -4,6 +4,7 @@ package com.workeache.precionline.api.demo.services;
 import com.workeache.precionline.api.demo.persistence.entities.DataApiRee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,12 @@ public class ApiReeServiceImpl implements ApiReeService{
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${precionline.ree.indicator.1001}")
+    private String INDICATOR_1001;
+
+    @Value("${precionline.ree.token}")
+    private String REE_TOKEN;
+
     @Override
     public DataApiRee updatePrices(LocalDate dateFrom, LocalDate dateTo) {
 
@@ -30,7 +37,7 @@ public class ApiReeServiceImpl implements ApiReeService{
 
         ResponseEntity<String> response =
                 restTemplate.exchange(
-                        "https://api.esios.ree.es/indicators/1001?start_date="+ dateFrom.format(formatDate) +"&end_date=" + dateTo.format(formatDate),
+                        INDICATOR_1001 + "?start_date="+ dateFrom.format(formatDate) +"&end_date=" + dateTo.format(formatDate),
                         HttpMethod.GET,
                         reeObjectHttpEntity,
                         String.class);
@@ -46,8 +53,7 @@ public class ApiReeServiceImpl implements ApiReeService{
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
-        //TODO Token a fichero de propiedades
-        headers.set("x-api-key", "608773a53bb93c375e5e5a1ea658f684df8b6777cec2c2d2c8e2602fbbabf4a0");
+        headers.set("x-api-key", REE_TOKEN);
 
         return headers;
     }
