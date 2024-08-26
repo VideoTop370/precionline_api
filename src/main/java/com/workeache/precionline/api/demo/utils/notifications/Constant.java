2c7956bb63f0419f9a9cc2a67e6d8679876e141e
agutierrez <andres6700@gmail.com>
2024-08-26 07:46:20 +0200

    Bug#003 -> Notificaciones Gotify, diferenciar entre manuales y automáticas

--- src/main/java/com/workeache/precionline/api/demo/utils/notifications/Constant.java
+++ src/main/java/com/workeache/precionline/api/demo/utils/notifications/Constant.java
@@ -1,9 +1,11 @@
 package com.workeache.precionline.api.demo.utils.notifications;
 
 public class Constant {
-    public static final Message A001_SUCCESS = new Message("Actualización A001","Precios día actual actualizados",1);
-    public static final Message A002_SUCCESS = new Message("Actualización A002","Precios día siguiente actualizados",1);
+    public static final Message A001_SUCCESS = new Message("Actualización A001","Precios día actual actualizados desde endpoint",1);
+    public static final Message A002_SUCCESS = new Message("Actualización A002","Precios día siguiente actualizados desde endpoint",1);
+    public static final Message A003_SUCCESS = new Message("Actualización A003","Precios día siguiente actualizados automáticamente",1);
 
-    public static final Message A001_ERROR = new Message("Error Act. A001","Error al actualizar los precios del día actual",1);
-    public static final Message A002_ERROR = new Message("Error Act. A002","Error al actualizar los precios del día actual",1);
+    public static final Message A001_ERROR = new Message("Error Act. A001","Error al actualizar los precios del día actual desde endpoint",1);
+    public static final Message A002_ERROR = new Message("Error Act. A002","Error al actualizar los precios del día actual desde endpoint",1);
+    public static final Message A003_ERROR = new Message("Error Act. A003","Error al actualizar los precios del día actual automáticamente",1);
 }
